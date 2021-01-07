71b4faf8e51d8d8d8a3400e70eee777ad03ee81d
Huỳnh Lâm Tứ <65360071+huynhlamtu4work@users.noreply.github.com>
2021-01-07 23:43:02 +0700

    Add Mobile app

---
+++ App1/App1/App1/Models/Dans.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App1.Models
+{
+    class Dans
+    {
+		public int ma_dan { get; set; }
+		public string email { get; set; }
+		public string pass_word { get; set; }
+		public string ho_ten { get; set; }
+		public string dia_chi { get; set; }
+		public string sdt { get; set; }
+		public string cmnd { get; set; }
+		public DateTime ngaysinh { get; set; }
+		public string gioi_tinh { get; set; }
+	}
+}
---
+++ App1/App1/App1/UtilsPages/Home.xaml.cs
@@ -0,0 +1,45 @@
+﻿using App1.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using App1.Data;
+
+
+namespace App1.UtilsPages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Home : ContentPage
+    {
+        //public ObservableCollection<mainNavItems> MySource { get; set; }
+        public Home()
+        {
+            InitializeComponent();
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            //List<Note> notes = await App.GetDatabase.GetNotesAsync();
+            //listView.ItemsSource = notes.OrderBy(d => d.Date).ToList();
+            getDanById();
+        }
+
+        async void getDanById()
+        {
+            Dans dan = await new DansWebService().GetDanById(Int16.Parse(Application.Current.Properties["userId"].ToString()));
+            userName.Text = "Welcome, " + dan.ho_ten;
+        }
+
+        void funcHandleNopPhat(object sender, EventArgs e)
+        {
+        }
+
+        void funcHandleSignOut(object sender, EventArgs e)
+        {
+        }
+    }
+}
